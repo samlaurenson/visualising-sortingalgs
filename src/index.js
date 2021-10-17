97b5e78b67a1dd2b894bf9bc5c46d5708157e702
Sam Laurenson <40398765@live.napier.ac.uk>
2021-10-17 17:18:34 +0100

    cleaning files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Toolbar from './Toolbar.jsx';
-import VisualiserGround from './valuevisualiser.jsx';
+
 
 //<Toolbar />
 //<App />
